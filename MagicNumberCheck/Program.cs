4fb3b891bcb22a7bacc087e96ce863db4e63d126
Brunine <yuzo.ananimation@gmail.m>
2023-06-08 02:21:09 -0300

    Adicionar arquivos de projeto.

---
+++ MagicNumberCheck/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class FileAnalyzer {
+    private static readonly string ConfigFilePath = "./extensions.json";
+    private static void Main(string[] args) {
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string fullPath = Path.Combine(currentDirectory, ConfigFilePath);
+        string texto1 = @"                  ,/////////.                   
+            //  /             @@@//             
+        ,/ /      @@@@@@@@@ /   &@/%@/          
+      / /                         @///&@/       
+    / / //@@@@%/ *   /////        @///@//@/     
+   / /   @@@@@@   @@@@@@@@@#     .@/#@/@/#@/    
+  / ,    @@@@                /  / @#@@/@@@&@/   
+ / /                          *  @(@@@/@@@@/@/  
+ ./                        // @@/@@@@@@@@@@@@/  
+. /             ./,  ,@@@@@@//@@@@@@@@@@@@@@/#  
+ ,/           / @@/%@@@@@@@@@@@@@@@@@@@@@@@&@/  
+ / /    *   / @/@@@@@@@@@@@@@@@@@@@@@@@@@@@/@/  
+  /        / @(@@/@@,@@@@@,@@@@@@@   %&@@@/@/   
+   / ./ /  ,@/@@@@(/       /(@@@      @@@/@/    
+    / */ /////@@@@@@@@#/#@@@@@@/,     @(@%*     
+      /  /// @@@@@@@@@@@@@@@@@@@@@@@@/@#/       
+         /  / .@@@#@       /#@@@@@/@@/          
+            */   @@@@@@@@@@@@#@@#/              
+                    ///////                     
+";
+        string texto2 = @"  ██▓███   ▒█████   ██▀███   ▄▄▄       ▒█████  
+ ▓██░  ██▒▒██▒  ██▒▓██ ▒ ██▒▒████▄    ▒██▒  ██▒
+ ▓██░ ██▓▒▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  ▒██░  ██▒
+ ▒██▄█▓▒ ▒▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██ ▒██   ██░
+ ▒██▒ ░  ░░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒░ ████▓▒░
+ ▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░▒░▒░ 
+ ░▒ ░       ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░  ░ ▒ ▒░ 
+ ░░       ░ ░ ░ ▒    ░░   ░   ░   ▒   ░ ░ ░ ▒  
+              ░ ░     ░           ░  ░    ░ ░  ";
+        Console.WriteLine(texto1);
+        Console.WriteLine(texto2);
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        if (!File.Exists(fullPath)) {
+            Console.WriteLine("Arquivo extensions.json não encontrado.");
+            return;
+        }
+        if (args.Length > 0){
+            string filePath = args[0];
+            if(File.Exists(filePath)){
+                string fileExtension = Path.GetExtension(filePath).TrimStart('.');
+                string fileMimeType = GetFileMimeType(filePath, fileExtension);
+                Console.WriteLine($"[*] O arquivo '{Path.GetFileName(filePath)}' é do tipo '{fileMimeType}'.");
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            }else{
+                Console.WriteLine("[*] O arquivo não existe.");
+                Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            }
+        }else{
+            Console.WriteLine("[*] Arraste um arquivo para cima do executável.");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        }
+        Console.ReadLine();
+    }
+    private static string GetFileMimeType(string filePath, string fileExtension) {
+        var config = LoadConfig();
+        if(config.TryGetValue(fileExtension, out var fileInfo)){
+            byte[] fileBytes = ReadFileBytes(filePath);
+            string fileSignature = BitConverter.ToString(fileBytes).Replace("-", "");
+            var signatures = fileInfo["signs"].ToObject<string[]>();
+            foreach(var signature in signatures){
+                string[] parts = signature.Split(',');
+                int offset = int.Parse(parts[0]);
+                string expectedSignature = parts[1];
+                if(fileSignature.Length >= (offset + expectedSignature.Length) &&
+                    fileSignature.Substring(offset, expectedSignature.Length) == expectedSignature) {
+                    return fileInfo["mime"].ToString();
+                }
+            }
+        }
+        return "Tipo de arquivo desconhecido";
+    }
+    private static byte[] ReadFileBytes(string filePath) {
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, bytes.Length);
+            return bytes;
+        }
+    }
+    private static JObject LoadConfig() {
+        string json = File.ReadAllText(ConfigFilePath);
+        return JsonConvert.DeserializeObject<JObject>(json);
+    }
+}
